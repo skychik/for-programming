c5a3da5a19896f9ffb0cb3c3ad316508c278e88d
sky <kir-esakov2011@mail.ru>
2017-03-21 18:40:04 +0300

    Merge branch 'master' of https://github.com/skychik/for-programming
    # Conflicts:
    #	.gitignore
    #	Laba2.Sasha/Laba2.Sasha.iml

--- src/main/java/ru/ifmo/cs/programming/lab5/core/NegativeSalaryException.java
+++ src/main/java/ru/ifmo/cs/programming/lab5/core/NegativeSalaryException.java
@@ -3,21 +3,10 @@ package ru.ifmo.cs.programming.lab5.core;
 /**
  * todo Исклюючение не содержит ничего нового. Зачем его добавлять?
  */
-public class NegativeSalaryException extends RuntimeException {
+public class NegativeSalaryException extends ArithmeticException {
 
-    /**
-     * Creates a new instance of <code>NegativeSalaryException</code> without
-     * detail message.
-     */
-    public NegativeSalaryException() {
-    }
+    public NegativeSalaryException(){}
 
-    /**
-     * Constructs an instance of <code>NegativeSalaryException</code> with the
-     * specified detail message.
-     *
-     * @param msg the detail message.
-     */
     public NegativeSalaryException(String msg) {
         super(msg);
     }
