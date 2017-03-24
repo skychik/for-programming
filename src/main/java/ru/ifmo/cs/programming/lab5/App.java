87b0daba63fe833c4669b32a7348e3cc60c5f80f
sky <kir-esakov2011@mail.ru>
2017-03-25 01:08:46 +0300

    Got rid of anonymous class Reading from command line supported

--- src/main/java/ru/ifmo/cs/programming/lab5/App.java
+++ src/main/java/ru/ifmo/cs/programming/lab5/App.java
@@ -8,23 +8,22 @@
 package ru.ifmo.cs.programming.lab5;
 
 import com.google.gson.Gson;
-import com.google.gson.stream.JsonReader;
 import ru.ifmo.cs.programming.lab5.domain.Employee;
-import ru.ifmo.cs.programming.lab5.domain.Product;
-import ru.ifmo.cs.programming.lab5.domain.ShopAssistant;
-import ru.ifmo.cs.programming.lab5.utils.AttitudeToBoss;
-import ru.ifmo.cs.programming.lab5.utils.FactoryWorker;
 
-import java.io.*;
+import java.io.FileReader;
+import java.io.InputStreamReader;
 import java.util.ArrayDeque;
 import java.util.Scanner;
+
+import static ru.ifmo.cs.programming.lab5.utils.AttitudeToBoss.LOW;
 
 public class App {
 
     public static void main(String[] args) throws Exception {
 
         String filePath = System.getenv("EmployeeFile");
-        BufferedReader fileReader = new BufferedReader(new FileReader(filePath));
+
+        //BufferedReader fileReader = new BufferedReader(new FileReader(filePath));
         /*
         String filePath = System.getenv("EmployeeFile");
         BufferedReader reader = new BufferedReader(new FileReader(filePath));
@@ -44,166 +43,233 @@ public class App {
         }
         */
 
-        ArrayDeque deque = new ArrayDeque<Employee>(){
-            void load() {
-                //todo Sasha: load
-                //initReader();
-                throw new UnsupportedOperationException();
-            }
+        ArrayDeque deque = new ArrayDeque<Employee>();
 
-            void save() {
-                //todo Sasha: save
-                throw new UnsupportedOperationException();
-            }
+        //First loading of the deque from our File
+        //load(deque);
 
-            void remove(Employee employee) {
-                throw new UnsupportedOperationException();
-            }
-
-            void remove_lower(Employee employee) {
-                throw new UnsupportedOperationException();
-            }
-
-            void remove_all(Employee employee) {
-                throw new UnsupportedOperationException();
-            }
-
-            //checks path to file with collection and file's existence
-            //нужен ли?
-            void initReader() {
-                try {
-                    String line = null;
-                    int index = 0;
-                    while ((line = fileReader.readLine()) != null) {
-                        Employee emp;             //  Создание объектов
-                        FactoryWorker fw;         //  Класса Employee или его наследников
-                        ShopAssistant shAs;       //
-                        ArrayDeque deq = new ArrayDeque<Employee>() {
-                        };
-                        Scanner sc = new Scanner(line);
-                        sc.useDelimiter(",");
-                        String next = sc.next();
-                        if (line.length() < 6) {
-                            System.out.println("Неверно задан объект в строке " + line + ". Описаны не все параметры.");
-                            System.exit(0);
-                        }
-                        try {
-                            String objClass = next;
-                            String name = next;
-                            String profession = next;
-                            Integer salary = Integer.parseInt(next);
-                            AttitudeToBoss attitudeToBoss = null;
-                            switch (next) {
-                                case "HATE": {
-                                    attitudeToBoss = AttitudeToBoss.HATE;
-                                    break;
-                                }
-                                case "LOW": {
-                                    attitudeToBoss = AttitudeToBoss.LOW;
-                                    break;
-                                }
-                                case "NORMAL": {
-                                    attitudeToBoss = AttitudeToBoss.NORMAL;
-                                    break;
-                                }
-                                case "HIGH": {
-                                    attitudeToBoss = AttitudeToBoss.HIGH;
-                                    break;
-                                }
-                                case "DEFAULT": {
-                                    attitudeToBoss = AttitudeToBoss.DEFAULT;
-                                    break;
-                                }
-                                default: {
-                                    System.out.println("Неверно указано значение в ячейке " + line + "D.");
-                                    System.exit(0);
-                                }
-                            }
-                            Byte workQuality = Byte.parseByte(next);
-                            switch (objClass) {
-                                case "FactoryWorker": {
-                                    fw = new FactoryWorker(name, profession, salary, attitudeToBoss, workQuality);
-                                    while (sc.hasNext()) try {
-                                        String[] name_and_price = next.split("- ");
-                                        Product product = new Product(name_and_price[0], Integer.parseInt(name_and_price[1]));
-                                        fw.getBagpack().add(product);
-                                    } catch (NumberFormatException e) {
-                                        System.out.println("Неверно задан формат предмета. В ячейке надо указать название продукта и цену через \"-\".");
-                                        System.exit(0);
-                                    }
-                                    deq.addFirst(fw);
-                                    break;
-                                }
-                                case "ShopAssistant": {
-                                    shAs = new ShopAssistant(name, profession, salary, attitudeToBoss, workQuality);
-                                    deq.addFirst(shAs);
-                                    break;
-                                }
-                                case "Employee": {
-                                    emp = new Employee(name, profession, salary, attitudeToBoss, workQuality);
-                                    deq.addFirst(emp);
-                                    break;
-                                }
-                                default: {
-                                    System.out.println("Приложение не обрабатывает указанный класс.");
-                                    System.exit(0);
-                                }
-                            }
-                        } catch (NumberFormatException e) {
-                            System.out.println("Исправьте значения в строке " + line + ". Столбцы C и E должны содержать числа.");
-                            System.exit(0);
-                        }
-                    }
-                    fileReader.close();
-                } catch (NullPointerException e) {
-                    System.out.println("Не существует переменной окружения EmployeeFile. Создайте её, " +
-                            "указав путь к файлу.");
-                    System.exit(0);
-                } catch (FileNotFoundException e) {
-                    System.out.println("Нет файла с данными о коллекции, путь к которому указан в переменной " +
-                            "окружения EmployeeFile");
-                    System.exit(0);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        };
-
-        deque.load();
-
-        InputStreamReader in = new InputStreamReader(System.in);
-        Scanner scanner = new Scanner(in).useDelimiter(" ");
-        Gson gson;
+        InputStreamReader in = new FileReader("src\\main\\java\\ru\\ifmo\\cs\\programming\\lab5\\input.txt")
+                /*InputStreamReader(System.in)*/; //input stream
+        Scanner scanner = new Scanner(in);//has to stop at enter
+        Gson gson = new Gson();
         String command;
 
+        Employee employee;
+        Employee employeeFromRemove = new Employee("Sasha", "programmer", 0, LOW, (byte) 4);
+
+        //todo Let gson.fromJson work with other classes, which extend Employee
         /*This is an interactive mode:*/
-        while (true) {
+        System.out.println("Write your command:");
+        intMode: while (true) {
             command = scanner.next();
-            Employee employee;
+            //System.out.println("!switch");
+            String obj;
             switch (command) {
                 case "remove":
-                    employee = gson.fromJson(in, Employee.class);
-                    deque.remove(employee);
+                    //System.out.println("command: \'" + command + '\'');
+                    obj = jsonFormatedObject(scanner);
+                    if (obj == null) continue;//if incorrect input: more closing brackets than opening
+                    //System.out.println(obj);
+                    //todo учитывать остаток после вычленения одной команты из потока ввода
+                    employee = gson.fromJson(obj, Employee.class);
+                    assert employee.equals(employeeFromRemove) : employee.toString();
+                    //remove(deque, employee);
                     break;
                 case "remove_lower":
-                    employee = gson.fromJson(in, Employee.class);
-                    deque.remove_lower(employee);
+                    //System.out.println("command: \'" + command + '\'');
+                    obj = jsonFormatedObject(scanner);
+                    if (obj == null) continue;//if incorrect input: more closing brackets than opening
+                    //System.out.println(obj);
+                    employee = gson.fromJson(obj, Employee.class);
+                    remove_lower(deque, employee);
                     break;
                 case "remove_all":
-                    employee = gson.fromJson(in, Employee.class);
-                    deque.remove_all(employee);
+                    //System.out.println("command: \'" + command + '\'');
+                    obj = jsonFormatedObject(scanner);
+                    if (obj == null) continue;//if incorrect input: more closing brackets than opening
+                    //System.out.println(obj);
+                    employee = gson.fromJson(obj, Employee.class);
+                    remove_all(deque, employee);
                     break;
                 case "save":
-                    deque.save();
+                    //System.out.println("command: \'" + command + '\'');
+                    save(deque);
                     break;
                 case "load":
-                    deque.load();
+                    //System.out.println("command: \'" + command + '\'');
+                    load(deque);
                     break;
-                case "end.":
-                    deque.save();
+                case "end":
+                    //System.out.println("command: \'" + command + '\'');
+                    //save(deque);
+                    break intMode;
+            }
+            //System.out.println("!end");
+        }
+    }
+
+    private static String jsonFormatedObject(Scanner scanner) {
+        int numberOfOpeningBrackets = 0;
+        int numberOfClosingBrackets = 0;
+        String obj = "";
+        String line;
+
+        while (true) {
+            //System.out.println('#');
+            line = scanner.nextLine();
+            obj += line.trim();
+            //countNumberOfBrackets
+            for (int pos = 0; pos < line.length(); ++pos)
+            {
+                char ch = line.charAt(pos);
+                // здесь делаем что хотим с символом
+                switch (ch){
+                    case '{':
+                        numberOfOpeningBrackets++;
+                        break;
+                    case '}':
+                        numberOfClosingBrackets++;
+                        break;
+                }
+            }
+
+            if (numberOfOpeningBrackets == numberOfClosingBrackets) return obj;
+                /*//Gson should do this itself
+                if (line.trim().toCharArray()[line.length() - 1] == '}')
                     return;
+                else {
+                    System.out.println("Wrong input format: didn't end with '}'");
+                    obj = null;
+                    return;
+                }*/
+            if (numberOfOpeningBrackets < numberOfClosingBrackets) {
+                System.out.println(
+                        "Number of closing figure brackets can't be more, than number of opening figure brackets");
+                return null;
             }
         }
     }
+
+    private static void load(ArrayDeque deque) {
+        //todo Sasha: load
+        //initReader();
+        throw new UnsupportedOperationException();
+    }
+
+    private static void save(ArrayDeque deque) {
+        //todo Sasha: save
+        throw new UnsupportedOperationException();
+    }
+
+    private static void remove(ArrayDeque deque, Employee employee) {
+        throw new UnsupportedOperationException();
+    }
+
+    private static void remove_lower(ArrayDeque deque, Employee employee) {
+        throw new UnsupportedOperationException();
+    }
+
+    private static void remove_all(ArrayDeque deque, Employee employee) {
+        throw new UnsupportedOperationException();
+    }
+
+    //checks path to file with collection and file's existence
+    //нужен ли?
+    /*static void initReader() {
+        try {
+            String line = null;
+            int index = 0;
+            while ((line = fileReader.readLine()) != null) {
+                Employee emp;             //  Создание объектов
+                FactoryWorker fw;         //  Класса Employee или его наследников
+                ShopAssistant shAs;       //
+                ArrayDeque deq = new ArrayDeque<Employee>();
+                Scanner sc = new Scanner(line);
+                sc.useDelimiter(",");
+                String next = sc.next();
+                if (line.length() < 6) {
+                    System.out.println("Неверно задан объект в строке " + line + ". Описаны не все параметры.");
+                    System.exit(0);
+                }
+                try {
+                    String objClass = next;
+                    String name = next;
+                    String profession = next;
+                    Integer salary = Integer.parseInt(next);
+                    AttitudeToBoss attitudeToBoss = null;
+                    switch (next) {
+                        case "HATE": {
+                            attitudeToBoss = AttitudeToBoss.HATE;
+                            break;
+                        }
+                        case "LOW": {
+                            attitudeToBoss = AttitudeToBoss.LOW;
+                            break;
+                        }
+                        case "NORMAL": {
+                            attitudeToBoss = AttitudeToBoss.NORMAL;
+                            break;
+                        }
+                        case "HIGH": {
+                            attitudeToBoss = AttitudeToBoss.HIGH;
+                            break;
+                        }
+                        case "DEFAULT": {
+                            attitudeToBoss = AttitudeToBoss.DEFAULT;
+                            break;
+                        }
+                        default: {
+                            System.out.println("Неверно указано значение в ячейке " + line + "D.");
+                            System.exit(0);
+                        }
+                    }
+                    Byte workQuality = Byte.parseByte(next);
+                    switch (objClass) {
+                        case "FactoryWorker": {
+                            fw = new FactoryWorker(name, profession, salary, attitudeToBoss, workQuality);
+                            while (sc.hasNext()) try {
+                                String[] name_and_price = next.split("- ");
+                                Product product = new Product(name_and_price[0], Integer.parseInt(name_and_price[1]));
+                                fw.getBagpack().add(product);
+                            } catch (NumberFormatException e) {
+                                System.out.println("Неверно задан формат предмета. В ячейке надо указать название продукта и цену через \"-\".");
+                                System.exit(0);
+                            }
+                            deq.addFirst(fw);
+                            break;
+                        }
+                        case "ShopAssistant": {
+                            shAs = new ShopAssistant(name, profession, salary, attitudeToBoss, workQuality);
+                            deq.addFirst(shAs);
+                            break;
+                        }
+                        case "Employee": {
+                            emp = new Employee(name, profession, salary, attitudeToBoss, workQuality);
+                            deq.addFirst(emp);
+                            break;
+                        }
+                        default: {
+                            System.out.println("Приложение не обрабатывает указанный класс.");
+                            System.exit(0);
+                        }
+                    }
+                } catch (NumberFormatException e) {
+                    System.out.println("Исправьте значения в строке " + line + ". Столбцы C и E должны содержать числа.");
+                    System.exit(0);
+                }
+            }
+            fileReader.close();
+        } catch (NullPointerException e) {
+            System.out.println("Не существует переменной окружения EmployeeFile. Создайте её, " +
+                    "указав путь к файлу.");
+            System.exit(0);
+        } catch (FileNotFoundException e) {
+            System.out.println("Нет файла с данными о коллекции, путь к которому указан в переменной " +
+                    "окружения EmployeeFile");
+            System.exit(0);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }*/
 }
 
