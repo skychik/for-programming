8465f11c6664f686333ea7bec129bb8041a896f6
sky <kir-esakov2011@mail.ru>
2017-04-03 13:33:26 +0300

    InteractiveMode now has more for methods from the main class Added command '--help'

--- src/main/java/ru/ifmo/cs/programming/lab5/App.java
+++ src/main/java/ru/ifmo/cs/programming/lab5/App.java
@@ -7,15 +7,10 @@
  */
 package ru.ifmo.cs.programming.lab5;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.JsonSyntaxException;
 import ru.ifmo.cs.programming.lab5.core.InteractiveModeFunctions;
 import ru.ifmo.cs.programming.lab5.domain.Employee;
-import ru.ifmo.cs.programming.lab5.utils.FactoryWorker;
 
 import java.io.File;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
 import java.util.Scanner;
@@ -27,12 +22,10 @@ public class App extends InteractiveModeFunctions {
     private static ArrayDeque<Employee> deque = new ArrayDeque<>();
     //for testing(changes to new FileReader(testingDir + "\\input.txt"))
     private static InputStreamReader inputStreamReader = new InputStreamReader(System.in);
-    //for working with Gson library
-    private static Gson gson = new GsonBuilder().setPrettyPrinting().serializeNulls().create();
-    private static Scanner scanner;//TODO: has to stop at enter
 
     public static void main(String[] args) throws Exception {
-        scanner = new Scanner(inputStreamReader).useDelimiter(Pattern.compile("[\\p{Space}\\r\\n\\u0085\\u2028\\u2029\\u0004]"));
+        //i'm not sure, than Pattern has to be that big, but it works
+        setScanner(new Scanner(inputStreamReader).useDelimiter(Pattern.compile("[\\p{Space}\\r\\n\\u0085\\u2028\\u2029\\u0004]")));
         if (getFilePath() == null)
             setFilePath(new File(System.getenv("EmployeeFile")));
 
@@ -49,66 +42,33 @@ public class App extends InteractiveModeFunctions {
     private static void interactiveMode() {
         String command = null;
 
-        System.out.println("** To stop this program, type 'end'\n");
-
+        System.out.println("** Type '--help' to see all commands\n" +
+                "** To stop this program, type 'end'\n");
         intMode:
         while (true) {
             System.out.print("Write your command:\n");
-            if (scanner.hasNextLine()) {
-                command = scanner.next();
+            if (getScanner().hasNextLine()) {
+                command = getScanner().next();
             } else {
                 System.exit(0);
             }
                 //System.out.println('\"' + command + '\"');
 
-            String obj;
-            Employee employee;
-
             switch (command) {
+                case "--help":
+                    help();
+                    break;
                 case "add":
-                    obj = jsonObject(scanner);
-                    if (obj == null) continue;//if incorrect input (more closing brackets than opening)
-                    //System.out.println(obj);
-                    try {
-                        employee = gson.fromJson(obj, Employee.class);
-                    } catch (JsonSyntaxException e) {
-                        System.out.println(e.getMessage());
-                        continue;
-                    }
-                    add(deque, employee);
+                    add(deque);
                     break;
                 case "remove":
-                    obj = jsonObject(scanner);
-                    if (obj == null) continue;//if incorrect input (more closing brackets than opening)
-                    //System.out.println(obj);
-                    //TODO: учитывать остаток после вычленения одной команды из потока ввода
-                    employee = gson.fromJson(obj, Employee.class);
-                    //assert employee.equals(new Employee("Sasha", "programmer", 1, LOW, (byte) 4)) :
-                    //        employee.toString();
-                    remove(deque, employee);
+                    remove(deque);
                     break;
                 case "remove_lower":
-                    obj = jsonObject(scanner);
-                    if (obj == null) continue;//if incorrect input (more closing brackets than opening)
-                    //System.out.println(obj);
-                    employee = gson.fromJson(obj, Employee.class);
-                    remove_lower(deque, employee);
+                    remove_lower(deque);
                     break;
                 case "remove_all":
-                    obj = jsonObject(scanner);
-                    if (obj == null) continue;//if incorrect input (more closing brackets than opening)
-                    //System.out.println(obj);
-                    Employee emp = gson.fromJson(obj, FactoryWorker.class);
-                    //ArrayDeque<Employee> arrayDeque = new ArrayDeque<>();
-                    //arrayDeque.add(new FactoryWorker("Pasha", "programmer", 18, NORMAL, (byte) 21));
-
-                    //assert (emp instanceof FactoryWorker);
-
-                    //assert emp.equals(arrayDeque.peekFirst()) :
-                    //        arrayDeque.peekFirst().toString() +
-                    //                '\n' +
-                    //                emp.toString();
-                    remove_all(deque, emp);
+                    remove_all(deque);
                     break;
                 case "save":
                     save(deque);
@@ -123,57 +83,6 @@ public class App extends InteractiveModeFunctions {
                     break intMode;
                 default:
                     System.out.println("No such command: '" + command + "'");
-            }
-        }
-    }
-
-    /**
-     * Returns String, which contains object in json format
-     *
-     * @param scanner has thread from System.in
-     * @return String which contains object in json format
-     */
-    private static String jsonObject(Scanner scanner) {
-        int numberOfOpeningBrackets = 0;
-        int numberOfClosingBrackets = 0;
-        StringBuilder obj = new StringBuilder();
-        String line;
-
-        while (true) {
-            //System.out.println('#');
-            line = scanner.nextLine();
-            obj.append(line.trim());
-
-            //countNumberOfBrackets
-            for (int pos = 0; pos < line.length(); ++pos) {
-                char ch = line.charAt(pos);
-
-                // здесь делаем что хотим с символом
-                switch (ch) {
-                    case '{':
-                        numberOfOpeningBrackets++;
-                        break;
-                    case '}':
-                        numberOfClosingBrackets++;
-                        break;
-                }
-            }
-
-            if ((numberOfOpeningBrackets == 0) && (numberOfClosingBrackets == 0))
-                    continue;
-            if (numberOfOpeningBrackets == numberOfClosingBrackets) return obj.toString();
-//                /*//Gson should do this itself
-//                if (line.trim().toCharArray()[line.length() - 1] == '}')
-//                    return;
-//                else {
-//                    System.out.println("Wrong input format: didn't end with '}'");
-//                    obj = null;
-//                    return;
-//                }*/
-            if (numberOfOpeningBrackets < numberOfClosingBrackets) {
-                System.out.println(
-                        "Number of closing figure brackets can't be more, than number of opening figure brackets");
-                return null;
             }
         }
     }
--- src/main/java/ru/ifmo/cs/programming/lab5/core/InteractiveModeFunctions.java
+++ src/main/java/ru/ifmo/cs/programming/lab5/core/InteractiveModeFunctions.java
@@ -1,13 +1,18 @@
 package ru.ifmo.cs.programming.lab5.core;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonSyntaxException;
 import org.supercsv.io.CsvBeanWriter;
 import org.supercsv.io.ICsvBeanWriter;
 import org.supercsv.prefs.CsvPreference;
 import ru.ifmo.cs.programming.lab5.domain.Employee;
+import ru.ifmo.cs.programming.lab5.utils.FactoryWorker;
 
 import java.io.*;
 import java.util.ArrayDeque;
 import java.util.Arrays;
+import java.util.Scanner;
 
 import static ru.ifmo.cs.programming.lab5.domain.Employee.parseEmployee;
 
@@ -16,14 +21,52 @@ public class InteractiveModeFunctions {
     /*файл, который хранит deque*/
     private static File filePath = null;
     private static int lineNumber = 1;
+    private static String obj;
+    private static Employee employee;
+    private static Scanner scanner;//TODO: has to stop at enter
+    //for working with Gson library
+    private static Gson gson = new GsonBuilder().setPrettyPrinting().serializeNulls().create();
+
+    /**
+     * Show out all supported commands
+     */
+    //TODO: make helping output
+    protected static void help() {
+        System.out.println("** You can use commands:\n" +
+                "add {}, remove {}, remove_lower {}, remove_all {}, save, load, show, end\n");
+    }
+
+    /**
+     * Add Employee to deque
+     *
+     * @param deque Works with this deque
+     */
+    protected static void add(ArrayDeque<Employee> deque) {
+        obj = jsonObject(scanner);
+        if (obj == null) return;//if incorrect input (more closing brackets than opening)
+        //System.out.println(obj);
+        try {
+            employee = gson.fromJson(obj, Employee.class);
+        } catch (JsonSyntaxException e) {
+            System.out.println(e.getMessage());
+            return;
+        }
+        deque.add(employee);
+    }
 
     /**
      * Removes employee from deque
      *
-     * @param deque    ArrayDeque
-     * @param employee Employee, that gotta be removed from deque
+     * @param deque ArrayDeque
      */
-    protected static void remove(ArrayDeque<Employee> deque, Employee employee) {
+    protected static void remove(ArrayDeque<Employee> deque) {
+        obj = jsonObject(scanner);
+        if (obj == null) return;//if incorrect input (more closing brackets than opening)
+        //System.out.println(obj);
+        //TODO: учитывать остаток после вычленения одной команды из потока ввода
+        employee = gson.fromJson(obj, Employee.class);
+        //assert employee.equals(new Employee("Sasha", "programmer", 1, LOW, (byte) 4)) :
+        //        employee.toString();
         deque.remove(employee);
         System.out.println("First employee, which you typed, removed");
     }
@@ -32,9 +75,12 @@ public class InteractiveModeFunctions {
      * Removes all employees from deque, which are lower(compares .toStrings), than this employee
      *
      * @param deque    ArrayDeque
-     * @param employee Employee
      */
-    protected static void remove_lower(ArrayDeque<Employee> deque, Employee employee) {
+    protected static void remove_lower(ArrayDeque<Employee> deque) {
+        obj = jsonObject(scanner);
+        if (obj == null) return;//if incorrect input (more closing brackets than opening)
+        //System.out.println(obj);
+        employee = gson.fromJson(obj, Employee.class);
         Arrays.sort(deque.toArray(new Employee[0]));
         while (!deque.isEmpty() && (employee.compareTo(deque.peekFirst())) > 0) {
             deque.removeFirst();
@@ -46,9 +92,21 @@ public class InteractiveModeFunctions {
      * Removes all Emloyees = this employee
      *
      * @param deque    ArrayDeque
-     * @param employee Employee
      */
-    protected static void remove_all(ArrayDeque<Employee> deque, Employee employee) {
+    protected static void remove_all(ArrayDeque<Employee> deque) {
+        obj = jsonObject(scanner);
+        if (obj == null) return;//if incorrect input (more closing brackets than opening)
+        //System.out.println(obj);
+        Employee emp = gson.fromJson(obj, FactoryWorker.class);
+        //ArrayDeque<Employee> arrayDeque = new ArrayDeque<>();
+        //arrayDeque.add(new FactoryWorker("Pasha", "programmer", 18, NORMAL, (byte) 21));
+
+        //assert (emp instanceof FactoryWorker);
+
+        //assert emp.equals(arrayDeque.peekFirst()) :
+        //        arrayDeque.peekFirst().toString() +
+        //                '\n' +
+        //                emp.toString();
         ArrayDeque<Employee> anotherDeque = new ArrayDeque<>();
         /*
           Puts all Employees < employee in anotherDeque
@@ -142,10 +200,11 @@ public class InteractiveModeFunctions {
         System.out.println("saved");
     }
 
-    protected static void add(ArrayDeque<Employee> deque, Employee employee) {
-        deque.add(employee);
-    }
-
+    /**
+     * Show all Employees from this deque
+     *
+     * @param deque Current deque
+     */
     protected static void show(ArrayDeque<Employee> deque) {
         System.out.println("Current employees now are the same with the file:");
         if (!deque.isEmpty()) {
@@ -156,14 +215,60 @@ public class InteractiveModeFunctions {
         System.out.println();
     }
 
-    public static void setFilePath(File filePath) {
-        //if (filePath)
-            InteractiveModeFunctions.filePath = filePath;
+    /**
+     * Returns String, which contains object in json format
+     *
+     * @param scanner has thread from System.in
+     * @return String which contains object in json format
+     */
+    private static String jsonObject(Scanner scanner) {
+        int numberOfOpeningBrackets = 0;
+        int numberOfClosingBrackets = 0;
+        StringBuilder obj = new StringBuilder();
+        String line;
+
+        while (true) {
+            //System.out.println('#');
+            line = scanner.nextLine();
+            obj.append(line.trim());
+
+            //countNumberOfBrackets
+            for (int pos = 0; pos < line.length(); ++pos) {
+                char ch = line.charAt(pos);
+
+                // здесь делаем что хотим с символом
+                switch (ch) {
+                    case '{':
+                        numberOfOpeningBrackets++;
+                        break;
+                    case '}':
+                        numberOfClosingBrackets++;
+                        break;
+                }
+            }
+
+            if ((numberOfOpeningBrackets == 0) && (numberOfClosingBrackets == 0))
+                continue;
+            if (numberOfOpeningBrackets == numberOfClosingBrackets) return obj.toString();
+//                /*//Gson should do this itself
+//                if (line.trim().toCharArray()[line.length() - 1] == '}')
+//                    return;
+//                else {
+//                    System.out.println("Wrong input format: didn't end with '}'");
+//                    obj = null;
+//                    return;
+//                }*/
+            if (numberOfOpeningBrackets < numberOfClosingBrackets) {
+                System.out.println(
+                        "Number of closing figure brackets can't be more, than number of opening figure brackets");
+                return null;
+            }
+        }
     }
 
-    public static File getFilePath() {
-        return filePath;
-    }
+    public static void setFilePath(File filePath) {InteractiveModeFunctions.filePath = filePath;}
+
+    public static File getFilePath() { return filePath; }
 
     /**
      * Метод, осуществляющий увеличение на единицу номера считываемой строки
@@ -180,6 +285,14 @@ public class InteractiveModeFunctions {
      * @author Zhurbova A.E.
      */
     public static int getLineNumber() {
-        return (lineNumber);
+        return lineNumber;
+    }
+
+    public static Scanner getScanner() {
+        return scanner;
+    }
+
+    public static void setScanner(Scanner scanner) {
+        InteractiveModeFunctions.scanner = scanner;
     }
 }
